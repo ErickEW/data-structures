7c084634051bf8225e37d572176d77c0e00cbb11
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-04-29 12:52:35 -0500

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace data_structures
+{
+    class Movie
+    {
+        public string name;
+
+        public int year;
+
+        public Movie (string name, int year)
+        {
+          this.name = name;
+          this.year = year;
+        }
+      
+    }
+}
